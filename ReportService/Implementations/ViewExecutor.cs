d28c966606df30fb1c26ef62741c696b99633cb4
Арсений Макаров <makarov.a@smartdriving.io>
2018-03-06 12:46:46 +0300

    Commit by sourcetree

--- ReportService/Implementations/ViewExecutor.cs
+++ ReportService/Implementations/ViewExecutor.cs
@@ -9,7 +9,7 @@ namespace ReportService.Implementations
 {
     public class ViewExecutor : IViewExecutor
     {
-        public string Execute(string viewTemplate, string json)
+        public virtual string Execute(string viewTemplate, string json)
         {
             TemplateServiceConfiguration templateConfig = new TemplateServiceConfiguration();
             templateConfig.DisableTempFileLocking = true;
@@ -37,7 +37,59 @@ namespace ReportService.Implementations
 
             return Engine.Razor.Run("somekey", null, model);
         }
-    }
+    }//class
+
+    public class TableViewExecutor : ViewExecutor, IViewExecutor
+    {
+        public override string Execute(string viewTemplate, string json)
+        {
+            return base.Execute(tableTemplate_, json);
+        }
+
+        private string tableTemplate_ = @"<!DOCTYPE html>
+ <html>
+ <head>
+ 
+     <title> Reports..</title>
+ 
+     <link rel = ""stylesheet"" href = ""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"">
+    
+        <style>
+            table {
+                border - collapse: collapse;
+                width: 100 %;
+            }
+
+            th, td {
+                border: 1px solid Black;
+                padding: 10px;
+            }
+    </style>
+</head>
+<body>
+    <h3 align = ""center""> Testing Razor </h3>
+     
+         <table class=""table table-bordered table-hover"">
+        <tr>
+            @foreach(var header in @Model.Headers)
+        {
+            <th> @header </th>
+            }
+        </tr>
+        @foreach(var props in @Model.Content)
+        {
+        <tr>
+            @foreach(var prop in @props)
+            {
+             <td> @prop </td>
+            }
+        </tr>
+        }
+    </table>
+</body>
+</html>";
+    }//class
+
 }
 
 
