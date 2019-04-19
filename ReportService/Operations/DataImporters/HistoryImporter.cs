4dc78a303f32273d6375462b0b6b0556cf550ac6
Арсений Макаров <makarov.a@smartdriving.io>
2019-04-19 16:18:38 +0300

    History importer added

---
+++ ReportService/Operations/DataImporters/HistoryImporter.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using ReportService.Interfaces.Core;
+using ReportService.Interfaces.ReportTask;
+
+namespace ReportService.Operations.DataImporters
+{
+    public class HistoryImporter: IOperation
+    {
+        public CommonOperationProperties Properties { get; set; } = new CommonOperationProperties();
+        private readonly IRepository repos;
+        private readonly IArchiver archiver;
+        public long OperInstanceId;
+
+        public HistoryImporter(IMapper mapper, HistoryImporterConfig config,
+            IRepository repository,IArchiver archiver)
+        {
+            mapper.Map(config, this);
+            mapper.Map(config, Properties);
+            Properties.NeedSavePackage = true;
+            repos = repository;
+            this.archiver = archiver;
+        }
+
+        public void Execute(IRTaskRunContext taskContext)
+        {
+            var query = $"select DataSet from [OperInstance] with(nolock) where id={OperInstanceId}";
+            var instance = repos.GetBaseQueryResult(query);
+
+                var package = OperationPackage.Parser.ParseFrom(archiver.ExtractFromByteArchive(instance as byte[]));
+                taskContext.Packages[Properties.PackageName] = package;
+        }
+
+        public Task ExecuteAsync(IRTaskRunContext taskContext)
+        {
+            Execute(taskContext);
+            return Task.CompletedTask;
+        }
+    }
+}
