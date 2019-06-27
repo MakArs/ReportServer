72eec279dff4735f129c62be4bd3e1f62c38ce4c
Арсений Макаров <makarov.a@smartdriving.io>
2019-06-27 13:01:07 +0300

    Added FtpExporter (functions same with ssh)

---
+++ ReportService/Operations/DataExporters/Configurations/FtpExporterConfig.cs
@@ -0,0 +1,24 @@
+﻿using ReportService.Interfaces.Operations;
+
+namespace ReportService.Operations.DataExporters.Configurations
+{
+    public class FtpExporterConfig : IExporterConfig
+    {
+        public string PackageName { get; set; }
+        public bool RunIfVoidPackage { get; set; }
+        public bool DateInName;
+        public string SourceFileFolder;
+        public string FileName;
+        public bool ConvertPackageToXlsx;
+        public bool ConvertPackageToJson;
+        public bool ConvertPackageToCsv;
+        //public bool ConvertPackageToXml;
+        public bool UseAllSets;
+        public string PackageRename;
+        public string Host;
+        public string Login;
+        public string Password;
+        public string FolderPath;
+        public int ClearInterval;
+    }
+}
