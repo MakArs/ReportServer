56ca3fe859527b3391104d547b3d62a493675024
borodachev <borodachev>
2021-10-20 12:21:44 +0300

    Add exception handling.

---
+++ ReportService.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Monik.Common;
+
+namespace ReportService.Api.Controllers
+{
+    [AllowAnonymous]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : BaseController
+    {
+        private readonly IMonik mMonik;
+
+        public ErrorsController(IMonik monik)
+        {
+            this.mMonik = monik;
+        }
+        
+        [Route("error")]
+        public void Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context.Error;
+        
+            mMonik.ApplicationError(exception.Message);
+            mMonik.ApplicationError(exception.StackTrace);
+        }
+    }
+}
